7631423124fbf1acf93987ea456b3b9b0bfe57a7
AlexeyBH <alexey_bh@mail.ru>
2022-02-07 18:28:16 +0300

    Initial Commit

---
+++ CatAward/CatAwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAwardApp.swift
+//  CatAward
+//
+//  Created by Alexey Khestanov on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatAwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
